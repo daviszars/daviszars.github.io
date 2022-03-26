a39416807639a266024a85052db012f569f02a31
Davis Zarins <davis.zars@gmail.com>
2022-03-26 18:21:50 +0200

    design and style changes

---
+++ index.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    $('.fadeonload').each( function(i){
+        $(this).animate({'opacity':'1','margin-left':'0px'},500);
+    });
+});
+    
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.fadein').each( function(i){
+            var bottom_of_element = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+                
+            if( bottom_of_window > bottom_of_element ){
+                $(this).animate({'opacity':'1'},500);
+            }
+        });
+    });
+});
